876af760228e2dec2deaebb637251e6966fe9ca3
THIAGO JOAQUIM MOTTA <thiago.motta@alunos.sc.senac.br>
2026-02-26 20:17:16 -0300

    teste

---
+++ src/main/java/br/senac/sc/Teste/API2/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package br.senac.sc.Teste.API2.controllers;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorld {
+
+    @GetMapping("/teste")
+    public ResponseEntity<String> teste() {
+        return ResponseEntity.ok("Olá Mundo");
+    }
+
+}
